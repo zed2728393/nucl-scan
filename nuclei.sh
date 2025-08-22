6abdc051e7cbc7acae23b3bd8d9da28a2130c060
zed2728393 <zed9992025@outlook.com>
2025-08-21 21:46:24 -0400

    Update nuclei.sh

--- nuclei.sh
+++ nuclei.sh
@@ -2,4 +2,6 @@
 
 echo "Starting Nuclei Scan on $1 "
 
-nuclei -u "https://www.$1" -rl 2 -bs 1 -c 2 -etags wp,wpscan,wp-plugin,iot,phishing, -s low,medium,high,critical -silent -o nuclei_results.txt
+rm -f nuclei_results.txt   
+
+nuclei -u "https://www.$1" -rl 2 -bs 1 -c 2 -etags wp,wpscan,wp-plugin,iot,phishing, -s low,medium,high,critical -o nuclei_results.txt
